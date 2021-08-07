cf66ada63ef6756cab2be4559d17dcd8d08a57b4
forex24 <admin@forex24.today>
2021-08-08 00:53:51 +0800

    clean m2/lib.rs

--- m2/src/lib.rs
+++ m2/src/lib.rs
@@ -5,6 +5,4 @@ pub mod analyzer;
 //pub mod seq_fx;
 //mod seq_fx_detector;
 //mod smain;
-mod seq2;
 mod sd3;
-mod fx;
--- m2/src/sd3.rs
+++ m2/src/sd3.rs
@@ -1,4 +1,3 @@
-use crate::seq2::Seq;
 use colored::*;
 use common::direction::Direction;
 use common::event::{PenEvent, SegmentEvent};
@@ -13,6 +12,44 @@ fn get_s(v: bool) -> &'static str {
         return "F";
     }
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct Seq {
+    high:f64,
+    low:f64
+}
+
+impl Seq {
+    pub fn new(from:f64, to:f64) -> Self { Self { 
+        high:f64::max(from, to),
+        low:f64::min(from, to)
+     } }
+
+    pub fn merge(&mut self, from:f64, to:f64, direction:Direction) -> bool {
+        let _high = f64::max(from, to);
+        let _low =f64::min(from, to);
+
+        let prev_include_next = self.high > _high && self.low < _low;
+        let next_include_prev = self.high < _high && self.low > _low;
+
+        if !prev_include_next && !next_include_prev {
+            return false;
+        }
+
+        match direction {
+            Direction::Up => {
+                self.high = f64::max(self.high, _high);
+                self.low = f64::max(self.low, _low);
+            },
+            Direction::Down => {
+                self.high = f64::min(self.high, _high);
+                self.low = f64::min(self.low, _low);
+            }
+        }
+        true
+    }
+}
+
 
 #[derive(Debug, Clone, Copy)]
 pub struct State {
